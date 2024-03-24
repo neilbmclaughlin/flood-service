e4809ebadb9f2fcec11ad958078544ed3f0905b0
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2024-03-24 13:49:18 +0000

    FSR-1170 | Add release workflow

---
+++ create-release-notes.js
@@ -0,0 +1,85 @@
+const fs = require('fs')
+const nunjucks = require('nunjucks')
+const yargs = require('yargs')
+
+function formatDate (inputDate) {
+  const [day, month, year] = inputDate.split('/').map(Number)
+
+  // Validate year, month, and day
+  if (isNaN(year) || isNaN(month) || isNaN(day) ||
+      month < 1 || month > 12 || day < 1 || day > new Date(year, month, 0).getDate()) {
+    return `${inputDate} (invalid date, please correct)`
+  }
+
+  const dateObj = new Date(year, month - 1, day)
+
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }
+
+  return dateObj.toLocaleDateString('en-GB', options)
+}
+
+const options = yargs
+  .option('f', {
+    alias: 'file',
+    describe: 'Input file path',
+    demandOption: true, // Make the option required
+    type: 'string' // Specify the type of the option's value
+  })
+  .option('o', {
+    alias: 'output',
+    describe: 'Output file',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('d', {
+    alias: 'date',
+    describe: 'Release date',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('r', {
+    alias: 'release',
+    describe: 'Release version',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('i', {
+    alias: 'id',
+    describe: 'Jira Release id',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('t', {
+    alias: 'template',
+    describe: 'Template file',
+    demandOption: true,
+    type: 'string'
+  })
+  .option('c', {
+    alias: 'dbChanges',
+    describe: 'Include database changes',
+    type: 'boolean',
+    default: false
+  })
+  .argv
+
+const templateStr = fs.readFileSync(options.template, 'utf8')
+const commitList = fs.readFileSync(options.file, 'utf8')
+const tickets = commitList.split('\n').filter(c => c.trim() !== '')
+
+const context = {
+  version: options.release,
+  date: formatDate(options.date),
+  id: options.id,
+  dbChanges: options.dbChanges,
+  tickets
+}
+
+const outputStr = nunjucks.renderString(templateStr, context)
+
+fs.writeFileSync(options.output, outputStr)
